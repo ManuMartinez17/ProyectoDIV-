fbb1b1d171a30f2e5b302d87f3e2072690c621bc
Erika Moreno <ejuliethmoreno@gmail.com>
2021-05-31 00:30:08 -0500

    Inicial

---
+++ ProyectoDIV1/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoDIV1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoDIV1.Models
+{
+    public class Usuario: BaseViewModel
+    {
+        #region Attributes
+        private string apodo;
+        private string password;
+        #endregion
+
+        #region Properties
+        public string Apodo
+        {
+            get { return this.apodo; }
+            set { SetValue(ref this.apodo, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        #endregion
+    }
+}
---
+++ ProyectoDIV1/ViewModels/InicioViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ProyectoDIV1.Models;
+using ProyectoDIV1.Views;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ProyectoDIV1.ViewModels
+{
+    public class InicioViewModel : Usuario
+    {
+        #region Commands
+        public Command LoginCommand { get; }
+
+        #endregion
+
+
+        public InicioViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+        // alertas del LoginPage
+        private async void OnLoginClicked(object obj)
+        {
+            if (string.IsNullOrEmpty(this.Apodo) || string.IsNullOrEmpty(this.Password))
+            {   
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar un correo electrónico.",
+                    "Aceptar");
+                return;
+            }
+       
+            await Application.Current.MainPage.Navigation.PushAsync(new inicioDiseno());
+        }
+       
+    }
+}
