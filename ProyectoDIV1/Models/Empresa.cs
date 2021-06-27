e6c84bd0f8067fa5774d30a0f618bf2593ee49c2
diegoparra <48141200+DiegoArturoParra@users.noreply.github.com>
2021-06-27 15:32:36 -0500

    modelo

--- ProyectoDIV1/Models/Empresa.cs
+++ ProyectoDIV1/Models/Empresa.cs
@@ -8,46 +8,11 @@ namespace ProyectoDIV1.Models
     {
         [PrimaryKey, AutoIncrement]
         public Guid UsuarioId { get; set; }
-        private string nombre;
-        private string nit;
-        private string email;
-        private string ciudad;
-        private string celular;
-        private string password;
-        public string Nombre
-        {
-            get { return this.nombre; }
-            set { SetValue(ref this.nombre, value); }
-        }
-
-        public string Password
-        {
-            get { return this.password; }
-            set { SetValue(ref this.password, value); }
-        }
-        public string Nit
-        {
-            get { return this.nit; }
-            set { SetValue(ref this.nit, value); }
-        }
-
-        public string Email
-        {
-            get { return this.email; }
-            set { SetValue(ref this.email, value); }
-        }
-
-        public string Ciudad
-        {
-            get { return this.ciudad; }
-            set { SetValue(ref this.ciudad, value); }
-        }
-
-        public string Celular
-        {
-            get { return this.celular; }
-            set { SetValue(ref this.celular, value); }
-        }
-
+        public string Nombre { get; set; }
+        public string Nit { get; set; }
+        public string Email { get; set; }
+        public string Ciudad { get; set; }
+        public string Departamento { get; set; }
+        public string Celular { get; set; }
     }
 }
