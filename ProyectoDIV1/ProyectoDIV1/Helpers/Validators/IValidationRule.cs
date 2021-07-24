fbaf776ce610df80805799fdc0f2d37e858484a2
diegoparra <48141200+DiegoArturoParra@users.noreply.github.com>
2021-07-24 00:41:06 -0500

    solucionando error

--- ProyectoDIV1/ProyectoDIV1/Helpers/Validators/IValidationRule.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace ProyectoDIV1.Validators
-{
-    public interface IValidationRule<T>
-    {
-        string ValidationMessage { get; set; }
-        bool Check(T value);
-    }
-}
